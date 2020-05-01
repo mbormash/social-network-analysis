fd2b9ace364b7407898f6269b58c63f1de5a46fe
Michael B <michael.b@implemica.com>
2020-05-01 03:00:31 +0300

    first commit

---
+++ src/main/java/bormashenko/michael/socialnetworkanalysis/SocialNetworkAnalysisApplication.java
@@ -0,0 +1,13 @@
+package bormashenko.michael.socialnetworkanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkAnalysisApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SocialNetworkAnalysisApplication.class, args);
+   }
+
+}
---
+++ src/test/java/bormashenko/michael/socialnetworkanalysis/SocialNetworkAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package bormashenko.michael.socialnetworkanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkAnalysisApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
